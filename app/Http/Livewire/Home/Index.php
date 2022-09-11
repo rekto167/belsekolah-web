9cdb64c6ae2983e314d3fa5d8067a8e2c33e6de5
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-09-11 16:33:05 +0700

    bismillah, sudah siap untuk alarm

--- app/Http/Livewire/Home/Index.php
+++ app/Http/Livewire/Home/Index.php
@@ -12,16 +12,38 @@ class Index extends Component
 {
     public $sekarang;
     public $day_id;
+    public $jam;
+    public $play;
+    public $listJadwal;
 
     public function mount(){
         $this->sekarang = Carbon::now()->translatedFormat('l, d M Y');
+        $this->play =false;
     }
 
     public function render()
     {
-        $schedules = Schedule::where('day_id', Carbon::now()->dayOfWeek)->get();
+        $this->jam = Carbon::now()->format('H:i');
+        $dayWeek = Carbon::now()->dayOfWeek;
+        if($dayWeek == 0){
+            $dayWeek = 7;
+        }
+        $schedules = Schedule::where('day_id', $dayWeek)->get();
         return view('livewire.home.index', [
             'schedules'=>$schedules,
         ]);
+    }
+
+    public function playkan(){
+        $jadwal = $this->listJadwal;
+        foreach($jadwal as $j){
+            if($j->time == Carbon::now()->format('H:m')){
+                $this->dispatchBrowserEvent('play', $j->id);
+            }
+        }
+    }
+
+    public function coba(){
+        dd('coba');
     }
 }
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,32 +1,79 @@
 <div>
     <h1>Aplikasi Bel Sekolah</h1>
-
-    <div class="bg-green-500 p-2 text-white">
-        <p>Alarm</p>
-    </div>
     <div wire:poll.1000ms>
-        <div x-timeout:1000="$el.innerText=$moment().format('LTS')">
-            <h3>{{ $sekarang }}</h3>
-        </div>
-    </div>
-    <table class="border-collapse border border-slate-500 mt-3">
-        <thead>
-            <tr>
-                <th class="border border-slate-600 p-2">#</th>
-                <th class="border border-slate-600 p-2">Hari</th>
-                <th class="border border-slate-600 p-2">Jam</th>
-                <th class="border border-slate-600 p-2">Kegiatan</th>
-            </tr>
-            @foreach ($schedules as $item)
+        <input x-timeout:1000="$el.innerText=$moment().format('H:m')" wire:model="jam">
+        <table class="border-collapse border border-slate-500 mt-3">
+            <thead>
                 <tr>
-                    <td class="border border-slate-700 p-2">{{ $loop->iteration }}</td>
-                    <td class="border border-slate-700 p-2">{{ $item->day->name }}</td>
-                    <td class="border border-slate-700 p-2">{{ $item->time }}</td>
-                    <td class="border border-slate-700 p-2">{{ $item->activity->name }}</td>
+                    <th class="border border-slate-600 p-2">#</th>
+                    <th class="border border-slate-600 p-2">Hari</th>
+                    <th class="border border-slate-600 p-2">Jam</th>
+                    <th class="border border-slate-600 p-2">Kegiatan</th>
+                    <th class="border border-slate-600 p-2">Sound</th>
                 </tr>
-            @endforeach
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($schedules as $item)
+                    <tr>
+                        <td class="border border-slate-700 p-2">{{ $loop->iteration }}</td>
+                        <td class="border border-slate-700 p-2">{{ $item->day->name }}</td>
+                        <td class="border border-slate-700 p-2">{{ $item->time }}</td>
+                        <td class="border border-slate-700 p-2">{{ $item->activity->name }}</td>
+                        <td class="border border-slate-700 p-2">
+                            <audio controls>
+                                <source src="{{ asset('/storage/sound/' . $item->activity->bell->name . '.mp3') }}"
+                                    type="audio/mpeg">
+                            </audio>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <script type="text/javascript">
+            let ONE_MINUTE = 60 * 1000;
+            let jam = moment().format('H:m');
+            let listAudio = document.querySelectorAll('audio');
+            let jadwal = null;
+            jadwal = @js($schedules);
+            const showTime = () => {
+                jam = moment().format('H:m');
+                jadwal.map((e, i) => {
+                    if (e.time == jam) {
+                        listAudio[i].play();
+                    }
+                })
+            }
+
+            function repeatEvery(func, interval) {
+                // Check current time and calculate the delay until next interval
+                var now = new Date(),
+                    delay = interval - now % interval;
+
+                function start() {
+                    // Execute function now...
+                    func();
+                    // ... and every interval
+                    setInterval(func, interval);
+                }
+
+                // Delay execution until it's an even interval
+                setTimeout(start, delay);
+            }
+
+            repeatEvery(showTime, ONE_MINUTE);
+            // let listAudio = document.querySelectorAll('audio');
+            // let jam = moment().format('H:m');
+            // let jadwal = null
+            // document.addEventListener('livewire:load', function() {
+            //     jadwal = @js($schedules);
+            //     jadwal.map((e, i) => {
+            //         setInterval(() => {
+            //             jam = moment().format('H:m')
+            //             if (e.time == jam) {
+            //                 listAudio[i].play();
+            //             }
+            //         }, 60000);
+            //     })
+            // })
+        </script>
+    </div>
